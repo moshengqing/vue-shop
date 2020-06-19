402013bad91d1146e9986759eecb1d8b0cd3b024
msq <2623903006@qq.com>
2020-06-19 16:10:48 +0800

    配置修改

---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="login_box">
+         <form action="">
+             <input type="text" name="" id="">
+             <input type="text" name="" id="">
+             <button>登录</button>
+         </form>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style  scoped>
+    .login_box{
+        position: fixed;
+        top:50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        height: 300px;
+        width: 500px;
+        background-color: antiquewhite;
+    }
+</style>
